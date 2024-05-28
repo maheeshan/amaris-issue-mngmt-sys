f74ceef3acaea823f5fa467b11150ddec6195739
Stefano Montrasio <stefano.montrasio@amaris.com>
2024-05-28 11:36:32 +0200

    Init commit

---
+++ src/main/java/com/amaris/gaimes/amaris_issue_mngmt_sys/AmarisIssueMngmtSysApplication.java
@@ -0,0 +1,13 @@
+package com.amaris.gaimes.amaris_issue_mngmt_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmarisIssueMngmtSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmarisIssueMngmtSysApplication.class, args);
+	}
+
+}
