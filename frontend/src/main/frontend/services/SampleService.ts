cf7dcdbd4aa2d022fbea880cd4ef3a26c6dd6759
Stefano Montrasio <stefano.montrasio@amaris.com>
2024-06-05 12:18:53 +0200

    Revert frontend module to isolate it in a new repo and run it on different server

--- frontend/src/main/frontend/services/SampleService.ts
+++
@@ -1,9 +0,0 @@
-export default async function getSampleString() {
-	return fetch("http://localhost:8080/sample/string", {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json"
-		}
-	}).then(response => response.json())
-	.catch(error => error);
-}
