44b254ef334241a55f3868e804889a1a048d1410
Stefano Montrasio <stefano.montrasio@amaris.com>
2024-06-06 17:54:45 +0200

    Call BE endpoint from FE

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,6 @@ import getSampleString from "@/services/SampleService";
 export default async function Home() {
 
   const response = await getSampleString();
-  console.log(response);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
--- frontend/services/SampleService.ts
+++ frontend/services/SampleService.ts
@@ -1,12 +1,17 @@
 export default async function getSampleString() {
-	return fetch("ai-issue-mngmt-backend:8080/sample/string", {
+	return fetch("http://ai-issue-mngmt-backend:8080/sample/string", {
 		method: "GET",
 		headers: {
 			"Content-Type": "application/json"
 		},
 		next: {
-			revalidate: 0
+			revalidate: 6
 		}
 	}).then(response => response.json())
-	.catch(error => error);
+	.catch(errorMsg => ({
+		data: null,
+		error: {
+			message: errorMsg
+		}
+	}));
 }
